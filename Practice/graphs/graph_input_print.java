81d6df8987cfec01071d15bc38a2bb315940c6af
Praveen Lohar <praveenlohar.in@gmail.com>
2022-11-24 09:26:08 +0530

    file added

---
+++ Practice/graphs/graph_input_print.java
@@ -0,0 +1,14 @@
+package graphs;
+
+import java.util.Scanner;
+
+public class graph_input_print {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		System.out.println("integer is" + " " + a);
+
+	}
+
+}
